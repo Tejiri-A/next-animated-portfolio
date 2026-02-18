0cb0d20254bcbf72340e1b58beb96cc11c3316d4
Oghenetejiri Samuel Amrasa <tjthecreator8@gmail.com>
2026-02-18 11:37:35 +0100

    Add interactive `Brain` component with `motion` animations and dynamic scroll-based rotations.

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+"use client";
+import { motion } from "motion/react";
 
 function ContactPage() {
   return (
-    <div>ContactPage</div>
+    <motion.div
+      className={"h-full"}
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >Contact</motion.div>
   );
 }
 
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+"use client"
+import {motion} from "motion/react"
 
 function PortfolioPage() {
   return (
-    <div>PortfolioPage</div>
+    <motion.div
+      className={"h-full"}
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >Portfolio</motion.div>
   );
 }
 
